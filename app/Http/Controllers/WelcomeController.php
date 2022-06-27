952d5ffb7de3678c92789460cd1b0f6acf079ed4
MBABAZI Jacques <71966425+devcron33886@users.noreply.github.com>
2022-06-27 09:23:57 +0200

    show categories

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -10,7 +10,7 @@ class WelcomeController extends Controller
 {
     public function __invoke()
     {
-        $categories=Category::paginate(5);
+        $categories=Category::all();
         $products=Product::with('variations')->paginate(6);
         return view('welcome',compact('categories','products'));
     }
