981e1fd96c9f56c58dd3bbee4aecab7ec2d50534
MBABAZI Jacques <71966425+devcron33886@users.noreply.github.com>
2022-06-30 16:51:07 +0200

    modified header

--- resources/views/livewire/navigation-component.blade.php
+++ resources/views/livewire/navigation-component.blade.php
@@ -8,8 +8,9 @@
                     <div
                         class="-ml-2 group relative bg-gray-900 border-transparent rounded-md focus-within:ring-2 focus-within:ring-white">
                         <div class="flex items-center space-x-6">
-                            <p class="text-sm font-bold text-white hover:text-gray-100">Tel: +250 789 814 205/+250 785 477 042</p>
+                            <p class="text-sm font-bold text-white hover:text-gray-100">Tel: +250784929046/+250785477042</p>
                             <p class="text-sm font-bold text-white hover:text-gray-100">WhatsApp: 0728177613</p>
+                            <p class="text-sm font-bold text-white hover:text-gray-100">frankuwuzuyinema@yahoo.fr</p>
                         </div>
                     </div>
 
