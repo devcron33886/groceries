1789faf09b22ce3a0d4f9aed8f51ef12d0b709dd
MBABAZI Jacques <71966425+devcron33886@users.noreply.github.com>
2022-07-01 11:36:02 +0200

    Added related products

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -6,11 +6,12 @@
             <livewire:product-gallery-component :product="$product"/>
 
             <!-- Product details -->
-            <div class="max-w-2xl mx-auto mt-14 sm:mt-16 lg:max-w-none lg:mt-0 lg:row-end-2 lg:row-span-2 lg:col-span-3">
+            <div
+                class="max-w-2xl mx-auto mt-14 sm:mt-16 lg:max-w-none lg:mt-0 lg:row-end-2 lg:row-span-2 lg:col-span-3">
                 <div class="flex flex-col-reverse">
                     <div class="mt-4">
                         <h1 class="text-2xl font-extrabold tracking-tight text-gray-900 sm:text-3xl">{{ $product->name }}
-                            </h1>
+                        </h1>
 
 
                     </div>
@@ -32,7 +33,6 @@
                 </div>
 
 
-
             </div>
 
 
@@ -43,7 +43,7 @@
             <div class="flex items-center justify-between space-x-4">
                 <h2 class="text-lg font-medium text-gray-900">Customers also viewed</h2>
                 <a href="{{ route('shop') }}"
-                    class="whitespace-nowrap text-sm font-medium text-indigo-600 hover:text-indigo-500">View all<span
+                   class="whitespace-nowrap text-sm font-medium text-indigo-600 hover:text-indigo-500">View all<span
                         aria-hidden="true"> &rarr;</span></a>
             </div>
             <div class="mt-6 grid grid-cols-1 gap-x-8 gap-y-8 sm:grid-cols-2 sm:gap-y-10 lg:grid-cols-4">
@@ -55,12 +55,13 @@
                                  class="object-center object-cover">
                             <div class="flex items-end opacity-0 p-4 group-hover:opacity-100" aria-hidden="true">
                                 <a href="{{ route('product-show',$product->slug) }}"
-                                    class="w-full bg-white bg-opacity-75 backdrop-filter backdrop-blur py-2 px-4 rounded-md text-sm font-medium text-gray-900 text-center">
+                                   class="w-full bg-white bg-opacity-75 backdrop-filter backdrop-blur py-2 px-4 rounded-md text-sm font-medium text-gray-900 text-center">
                                     View Product
                                 </a>
                             </div>
                         </div>
-                        <div class="mt-4 flex items-center justify-between text-base font-medium text-gray-900 space-x-8">
+                        <div
+                            class="mt-4 flex items-center justify-between text-base font-medium text-gray-900 space-x-8">
                             <h3>
                                 <a href="{{ route('product-show',$product->slug) }}">
                                     <span aria-hidden="true" class="absolute inset-0"></span>
